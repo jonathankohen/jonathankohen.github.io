38460b907a19abe29558a521e660ba17a724727b
Jon Kohen <jonkohen@Jons-Mac-mini.home>
2019-12-10 23:14:09 -0500

    First commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+function initMap() {
+    // The location of Uluru
+    var loc = {lat: 40.8054491, lng: -73.9654415};
+    // The map, centered at Uluru
+    var map = new google.maps.Map(
+        document.getElementById('map'), {zoom: 4, center: loc});
+    // The marker, positioned at Uluru
+  }
