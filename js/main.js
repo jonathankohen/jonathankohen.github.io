fcc403204a9ebaed20aa133d4d6de97a339570bf
Jon Kohen <jonkohen@Jons-Mac-mini.home>
2020-01-17 16:23:01 -0500

    Fixed reservations JS

--- js/main.js
+++ js/main.js
@@ -3,17 +3,22 @@ function bleh() {
   var name = document.getElementById("name").value;
   var date = document.getElementById("date").value;
   var newReservation = document.createElement("tr");
-  newReservation.innerHTML(`<td>${name}</td><td>${date}</td>`);
-      
+
+  newReservation.innerHTML = `<td>${name}</td><td>${date}</td>`;
+
   reservations.appendChild(newReservation);
 }
 
+document.getElementById("subtmit-button").onclick = bleh()
+
 function initMap() {
   // The location of Uluru
-  var location = {lat: 28.3852, lng: -81.5639};
+  var location = { lat: 28.3852, lng: -81.5639 };
   // The map, centered at Uluru
-  var map = new google.maps.Map(
-      document.getElementById('map'), {zoom: 14, center: location});
+  var map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 14,
+    center: location
+  });
   // The marker, positioned at Uluru
-  var marker = new google.maps.Marker({position: location, map: map});
-}
+  var marker = new google.maps.Marker({ position: location, map: map });
+};
