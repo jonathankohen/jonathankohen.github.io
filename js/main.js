ffa9ec2825db7fb4e45a4229a7fe331d1debec03
Jon Kohen <jonkohen@Jons-Mac-mini.home>
2020-01-17 15:35:12 -0500

    Started over

--- js/main.js
+++ js/main.js
@@ -1,20 +1,19 @@
-function bleh(e) {
-  e.preventDefault();
+function bleh() {
   var reservations = document.getElementById("bleh-body");
   var name = document.getElementById("name").value;
   var date = document.getElementById("date").value;
   var newReservation = document.createElement("tr");
-  newReservation.innerHTML(`<td class="data table-hover table-light">${name}</td>
-      <td class="data table-hover table-light">${date}</td>`);
+  newReservation.innerHTML(`<td>${name}</td><td>${date}</td>`);
+      
   reservations.appendChild(newReservation);
 }
 
 function initMap() {
   // The location of Uluru
-  var location = {lat: 40.8054491, lng: -73.9654415};
+  var location = {lat: 28.3852, lng: -81.5639};
   // The map, centered at Uluru
   var map = new google.maps.Map(
-      document.getElementById('map'), {zoom: 7, center: location});
+      document.getElementById('map'), {zoom: 14, center: location});
   // The marker, positioned at Uluru
   var marker = new google.maps.Marker({position: location, map: map});
 }
