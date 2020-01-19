f6bbd8d128f88c880b1f585f807d6d8073af9f59
Jon Kohen <jonkohen@Jons-Mac-mini.home>
2020-01-19 06:57:59 -0500

    Code City

--- js/index.js
+++ js/index.js
@@ -1,28 +1,28 @@
-/*jshint esversion: 6 */
+var events = require('events');
+var eventEmitter = new events.EventEmitter();
 
-module.exports = {
-  printMsg: function() {
-    console.log("This is a message from the demo package");
-  }
-};
-
-// Load HTTP module
-const http = require("http");
-const hostname = "127.0.0.1";
-const port = 8000;
-// Create HTTP server
-const server = http.createServer((req, res) => {
-  // Set the response HTTP header with HTTP status and Content type
-  res.writeHead(200, { "Content-Type": "text/plain" });
-
-  // Send the response body "Hello World"
-  res.end("Hello World\n");
+eventEmitter.on('scream', function() {
+console.log('A scream is detected!');
 });
+eventEmitter.emit('scream');
 
-// Prints a log once the server starts listening
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+// // Load HTTP module
+// const http = require("http");
+// const hostname = "127.0.0.1";
+// const port = 8000;
+// // Create HTTP server
+// const server = http.createServer((req, res) => {
+//   // Set the response HTTP header with HTTP status and Content type
+//   res.writeHead(200, { "Content-Type": "text/plain" });
+
+//   // Send the response body "Hello World"
+//   res.end("Hello World\n");
+// });
+
+// // Prints a log once the server starts listening
+// server.listen(port, hostname, () => {
+//   console.log(`Server running at http://${hostname}:${port}/`);
+// });
 
 // const MongoClient = require("mongodb").MongoClient;
 // const uri =
--- js/main.js
+++ js/main.js
@@ -1,36 +1,50 @@
 /*jshint esversion: 6 */
 
-function bleh() {
-  const name = document.getElementById("name").value;
-  console.log(name);
-  const date = document.getElementById("date").value;
-  console.log(date);
+/*global let name = document.getElementById("name").value;*/
 
-  const reservationsTable = document.getElementById("bleh-body");
-  console.log(reservationsTable);
-  const newTableRow = document.createElement("tr");
-  console.log(newTableRow);
+function bleh() {
+  
+  let date = document.getElementById("date").value;
+  let reservationsTable = document.getElementById("bleh-body");
+  let newTableRow = document.createElement("tr");
   newTableRow.setAttribute("class", "submitted");
-  console.log(newTableRow);
   newTableRow.innerHTML = `<td class="reservation-names submitted-name">${name}</td><td class="submitted-date">${date}</td>`;
-  console.log(newTableRow);
 
   reservationsTable.appendChild(newTableRow);
-  console.log(reservationsTable);
 }
 
-let MongoClient = require('mongodb').MongoClient;
-MongoClient.connect("mongodb+srv://jkohen:KesheT123@cluster0-nflko.gcp.mongodb.net/test?retryWrites=true&w=majority", function(err, client){
-   if(err) throw err;
-   
-   let db = client.db('reservations');
-   db.collection('userInput').find().toArray(function(err, result){
-     if(err) throw err;
-     console.log(result);
-     client.close();
-   });
-});
+bleh();
 
+// let MongoClient = require("mongodb").MongoClient;
+// MongoClient.connect(
+//   "mongodb+srv://jkohen:KesheT123@cluster0-nflko.gcp.mongodb.net/test?retryWrites=true&w=majority",
+//   function(err, client) {
+//     if (err) throw err;
+
+//     let db = client.db("reservations");
+//     db.collection("userInput")
+//       .find()
+//       .toArray(function(err, result) {
+//         if (err) throw err;
+//         console.log(result);
+//         client.close();
+//       });
+//   }
+// );
+
+var MongoClient = require ('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/";
+
+MongoClient.connect(url, function(err, db) {
+  if (err) throw err;
+  var dbo = db.db("mydb");
+  var myobj = { name: "Company Inc", address: "Highway 37" };
+  dbo.collection("customers").insertOne(myobj, function(err, res) {
+    if (err) throw err;
+    console.log("1 document inserted");
+    db.close();
+  });
+});
 
 // const uri =
 //   "mongodb+srv://jkohen:KesheT123@cluster0-nflko.gcp.mongodb.net/test?retryWrites=true&w=majority";
