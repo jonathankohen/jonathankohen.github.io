8542bede25123a6ba6001c858372c8e8dfca1efe
Jon Kohen <jonkohen@Jons-Mac-mini.home>
2020-01-19 12:32:51 -0500

    added es6 w/express

---
+++ js/express_restaurant_project/src/routes/index.js
@@ -0,0 +1,15 @@
+// routes/index.js
+import express from 'express';
+var router = express.Router();
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+export default router;
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+module.exports = router;
