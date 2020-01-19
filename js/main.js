71fad493f89df10d945a004b6b9c08f396b947e3
Jon Kohen <jonkohen@Jons-Mac-mini.home>
2020-01-19 03:42:55 -0500

    Added Node to js

--- js/main.js
+++ js/main.js
@@ -1,32 +1,22 @@
-var http = require('http');
-
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('Hello World!');
-}).listen(8080);
-
-const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb+srv://jkohen:KesheT123@cluster0-nflko.gcp.mongodb.net/test?retryWrites=true&w=majority";
-var client = new MongoClient(uri, { useNewUrlParser: true });
-client.connect(err => {
-  const collection = client.db("reservations").collection("userInput");
-  // perform actions on the collection object
-  collection.insertOne({
-    name: name,
-    date: date
-  });
-  client.close();
-});
-
-
-document.getElementById("subtmit-button").onclick = bleh();
+/*jshint esversion: 6 */
 
 function bleh() {
-  const reservationsTable = document.getElementById("bleh-body");
   const name = document.getElementById("name").value;
+  console.log(name);
   const date = document.getElementById("date").value;
-  const newReservation = document.createElement("tr");
-  newReservation.setAttribute("class", "submitted");
-  newReservation.innerHTML = `<td class="reservation-names submitted-name">${name}</td><td class="submitted-date">${date}</td>`;
-  reservationsTable.appendChild(newReservation);
+  console.log(date);
+
+  const reservationsTable = document.getElementById("bleh-body");
+  console.log(reservationsTable);
+  const newTableRow = document.createElement("tr");
+  console.log(newTableRow);
+  newTableRow.setAttribute("class", "submitted");
+  console.log(newTableRow);
+  newTableRow.innerHTML = `<td class="reservation-names submitted-name">${name}</td><td class="submitted-date">${date}</td>`;
+  console.log(newTableRow);
+
+  reservationsTable.appendChild(newTableRow);
+  console.log(reservationsTable);
 }
+
+document.getElementById("#subtmit-button").addEventListener("click", bleh());
