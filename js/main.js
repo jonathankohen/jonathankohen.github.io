745fe9359caf01cb56ffb362dbc41c172df6082a
Jon Kohen <jonkohen@Jons-Mac-mini.home>
2020-02-13 14:33:24 -0500

    Alert test

--- js/main.js
+++ js/main.js
@@ -7,9 +7,7 @@ function handleSubmit() {
   const inputDate = document.getElementById("datepicker").value;
   const inputTime = document.getElementById("inputTime").value;
 
-  var newDiv = document.createElement("div");
-
-  var newContent = `<h2>Thank you, ${inputName}!</h2><p>Your reservation for ${inputDate} at ${inputTime} has been confirmed.</p>`;
-
-  newDiv.innerHTML = newContent;
+  alert(
+    `<h2>Thank you, ${inputName}!</h2><p>Your reservation for ${inputDate} at ${inputTime} has been confirmed.</p>`
+  );
 }
